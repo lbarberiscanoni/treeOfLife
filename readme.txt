18476c20d76476c2686b10fe2ffca74bf125019d
lbarberiscanoni <hllbck7@gmail.com>
2016-10-17 23:11:50 -0700

    initial commit

---
+++ readme.txt
@@ -0,0 +1,1 @@
+A "virus" that downloads the tree commmand and gets the entire file tree from a user's OS
---
+++ treeOfLife.py
@@ -0,0 +1,39 @@
+import platform
+import subprocess
+
+def selfdestruct():
+    pathToSelf = subprocess.check_output("locate treeOfLife.py", shell=True)
+    subprocess.call("rm -rf " + str(pathToSelf), shell=True)
+
+def checkForHomebrew():
+    brewCheck = ""
+    try:
+        brewCheck = subprocess.check_output("which brew", shell=True)
+    except:
+        brewCheck = "no brew"
+
+    return brewCheck
+
+def checkForTree():
+    treeCheck = ""
+    try:
+        treeCheck = subprocess.call("which tree", shell=True)
+    except:
+        treeCheck = "no tree"
+
+    return tree
+
+def getSystem(): 
+    return platform.system()
+
+if checkForTree() == "no tree":
+    if getSystem() == "Linux":
+        subprocess.call("sudo apt-get install tree", shell=True)
+    else if getSystem() == "Mac":
+        if checkForHomebrew() == "no brew":
+            getBrewCommand = "/usr/bin/ruby -e '$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)'"
+            subprocess.call(getBrewCommand, shell=True)
+        else:
+            subprocess.call("brew install tree", shell=True)
+    else:
+        selfdestruct()
